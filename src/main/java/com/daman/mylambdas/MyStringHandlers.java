1442d0e74e2176160bf56ab5dcd2150fa635d955
Daman Camara <dcamara3@gmail.com>
2021-08-04 01:50:23 -0400

    committing the changes

---
+++ src/main/java/com/daman/mylambdas/MyStringHandlers.java
@@ -0,0 +1,7 @@
+package com.daman.mylambdas;
+
+import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
+
+public class MyStringHandlers extends SpringBootRequestHandler<String, String> {
+
+}
---
+++ src/test/java/com/daman/mylambdas/SampleLambdaApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.daman.mylambdas;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SampleLambdaApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
